427fb14efc209853e2ef63cd41eede3421daf8b5
Justin Liu <justin_liu@hotmail.co.uk>
2018-05-16 11:19:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function updateObjectWithKeyAndValue(object, key, value){
 
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   object[key]=value;
-  return object
+  return object;
 }
