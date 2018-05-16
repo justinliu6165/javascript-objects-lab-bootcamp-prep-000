16c091f85eb7ea0ad7bee0bdb3037c15262ef406
Justin Liu <justin_liu@hotmail.co.uk>
2018-05-16 11:31:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function deleteFromObjectByKey(object,key){
   return newObject
 }
 
-function destructivelyDeleteObjectByKey(object, key){
+function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key];
   return object;
 }
